4da650354afef1f159e2a1c72de142211a9f9754
MuchanKim <mutopia82@gmail.com>
2024-10-18 15:46:58 +0900

    Initial Commit

---
+++ MoveApp/MoveAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoveAppApp.swift
+//  MoveApp
+//
+//  Created by Foundation on 10/18/24.
+//
+
+import SwiftUI
+
+@main
+struct MoveAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
